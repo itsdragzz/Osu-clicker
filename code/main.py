8c3be4ccecf493b0c846c724351bcea6ef5b3981
JohnDoe102 <hieuledang3110@gmail.com>
2022-09-18 20:20:02 +1000

    Basic circle animation
    Circle grows and shrinks in size after each click. Can adjust position of the circle later.

--- code/main.py
+++ code/main.py
@@ -1,26 +1,38 @@
 import pygame, sys
 from settings import *
-from level import Level
 
-class Game:
-	def __init__(self):
-		pygame.init()
-		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-		pygame.display.set_caption('Sprout land')
-		self.clock = pygame.time.Clock()
-		self.level = Level()
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+fps_clock = pygame.time.Clock()
 
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-  
-			dt = self.clock.tick() / 1000
-			self.level.run(dt)
-			pygame.display.update()
+# osu cirlce
+osu_size = 100
+osu_surf_old = pygame.image.load('images/Osulogo.png').convert_alpha()
+osu_surf = pygame.transform.scale(osu_surf_old, (osu_size, osu_size))
+osu_rect = osu_surf.get_rect(center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+acce = 0
 
-if __name__ == '__main__':
-	game = Game()
-	game.run()
+while (True):
+	for event in pygame.event.get():
+		# if function to close the program
+		if (event.type == pygame.QUIT):
+			pygame.quit()
+			sys.exit()
+
+		if (event.type == pygame.MOUSEBUTTONDOWN):
+			if (osu_rect.collidepoint(event.pos)):
+				print('clicked')
+				osu_size = 120
+				acce = 0
+				print(osu_size)
+
+	# Circle
+	screen.blit(osu_surf,osu_rect)
+	acce -= 1
+	osu_size += acce	
+	if (osu_size <= 100): osu_size = 100
+
+	osu_surf = pygame.transform.scale(osu_surf_old, (osu_size, osu_size))
+	osu_rect = osu_surf.get_rect(center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+
+	pygame.display.update()
+	fps_clock.tick(60)
